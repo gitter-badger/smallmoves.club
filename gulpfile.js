812effa65d523097326cc480e9d0ccfd807a8888
Joost Schuur <jschuur@jschuur.com>
2015-05-11 22:24:01 +0100

    Build sitemap.xml via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass');
+var gulp    = require('gulp'),
+    sass    = require('gulp-ruby-sass'),
+    sitemap = require('gulp-sitemap');
 
 gulp.task('styles', function() {
 	return sass('sass/style.scss', { style: 'nested' })
 	       .pipe(gulp.dest('css'));
+});
+
+gulp.task('sitemap', function () {
+	return gulp.src('index.html')
+	       .pipe(sitemap({
+	        	siteUrl: 'http://smallmoves.club'
+	        }))
+           .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch',  function() {
