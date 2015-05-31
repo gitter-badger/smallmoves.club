e3ce3aa6aa2ca3be663f5824c04fdef66e10b61c
Joost Schuur <jschuur@jschuur.com>
2015-05-31 18:34:17 +0100

    API endpoint to update Member.joined_slack

--- app/routes/api.js
+++ app/routes/api.js
@@ -14,4 +14,24 @@ router.get('/members.json', function (request, response) {
 	});
 });
 
+router.post('/joined_slack', function (request, response) {
+	if(helpers.api_authenticated(request.body.api_key))
+		if(request.body.email)
+			Member.findOne({ email: request.body.email }).exec(function (err, member) {
+				if(err)
+					response.status(500).json({ error: 'Error running query: ' + err });
+				else if(member) {
+					member.joined_slack = true;
+					member.save();
+
+					response.status(200).json({ message: 'Member join status updated' });
+				} else
+					response.status(404).json({ error: 'Unknown member' });
+			});
+		else
+			response.status(400).json({ error: 'Missing parameter (email)' });
+	else
+		response.status(403).json({ error: 'Invalid API key' });
+});
+
 module.exports = router;
